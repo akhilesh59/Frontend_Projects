c39630cceedcc681c4b817b78f2cddfaa6bf6323
Akhilesh Singh <akhisingh6871@gmail.com>
2025-01-23 23:15:10 +0530

    Projects completed. Pushing on Github

---
+++ Shopping-App/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Spinner = () => {
+  return (
+    <div className='h-full flex w-screen justify-center items-center'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
